585e3df434a0f0057f79f1cb97259f92ac04f456
Daniel Waiguru <danielirungu91@gmail.com>
2020-08-29 02:19:22 +0300

    add Splash Screen

---
+++ app/src/main/java/tech/danielwaiguru/gads2020/SplashActivity.kt
@@ -0,0 +1,16 @@
+package tech.danielwaiguru.gads2020
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
