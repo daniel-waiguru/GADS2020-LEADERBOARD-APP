66efe3e5e45ccefdb7b18c844117f3d027240ca5
Daniel Waiguru <danielirungu91@gmail.com>
2020-08-29 19:45:53 +0300

    add IQLeaderViewModel

--- app/src/main/java/tech/danielwaiguru/gads2020/ui/viewmodels/IQLeaderViewModel.kt
+++ app/src/main/java/tech/danielwaiguru/gads2020/ui/viewmodels/IQLeaderViewModel.kt
@@ -8,28 +8,28 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import tech.danielwaiguru.gads2020.models.LearningLeader
 import tech.danielwaiguru.gads2020.models.Resource
+import tech.danielwaiguru.gads2020.models.SkillIQLeader
 import tech.danielwaiguru.gads2020.repositories.MainRepository
 
-class LearningLeaderViewModel
-@ViewModelInject constructor(private val mainRepository: MainRepository): ViewModel() {
+class IQLeaderViewModel
+@ViewModelInject constructor(private val mainRepository: MainRepository) : ViewModel(){
     private val _toast: MutableLiveData<String> = MutableLiveData()
     val toast : LiveData<String>
-    get() = _toast
-    private val _learningLeaders: MutableLiveData<List<LearningLeader>> = MutableLiveData()
-    val learningLeaders : LiveData<List<LearningLeader>>
-    get() = _learningLeaders
-
+        get() = _toast
+    private val _learningLeaders: MutableLiveData<List<SkillIQLeader>> = MutableLiveData()
+    val learningLeaders : LiveData<List<SkillIQLeader>>
+        get() = _learningLeaders
     /**
      * get Learning Leaders and update leadingLeaders LiveData
      */
-    fun fetchLearningLeaders(){
+    fun fetchIQLeaders(){
         viewModelScope.launch {
-            when (val result = mainRepository.fetchLearningLeaders()){
-                is Resource.Success -> {
-                    _learningLeaders.value = result.data
-                }
+            when (val result = mainRepository.fetchIQLeaders()){
                 is Resource.Loading -> {
 
+                }
+                is Resource.Success -> {
+                    _learningLeaders.value = result.data
                 }
                 is Resource.Error -> {
                     _toast.value = result.message
@@ -37,5 +37,4 @@ class LearningLeaderViewModel
             }
         }
     }
-
 }
